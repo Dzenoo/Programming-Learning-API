651bf4235f892ffaefba601c7736b4a5606dd9e2
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-03-17 16:56:27 +0100

    asd

--- models/SubmittedChallenge.js
+++ models/SubmittedChallenge.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SubmitSchema = new Schema({
-  title: { type: "String", required: true },
-  github: { type: "String", required: true },
-  site: { type: "String", required: true },
-  description: { type: "String", required: true },
-  submitter: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-});
+const SubmitSchema = new Schema(
+  {
+    title: { type: "String", required: true },
+    github: { type: "String", required: true },
+    site: { type: "String", required: true },
+    description: { type: "String", required: true },
+    submitter: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("SubmitChallenge", SubmitSchema);
